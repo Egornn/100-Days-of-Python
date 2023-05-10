5ec16b2b4dec17e2605d8031597b289efd0c435b
Egor Nazdryuhin <egornnn@gmail.com>
2023-05-11 00:40:29 +0300

    Day1: Band Name Generator

---
+++ Begginer/Band Name Generator/main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+
+city = input('Welcome to the Band Name Generator!\nWhat is the city of your origin? ')
+pet = input('What is the name of your pet? ')
+print(f'Your Band Name is {city} {pet}')
