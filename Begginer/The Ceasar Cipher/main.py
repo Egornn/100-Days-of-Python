3bbf7d70f67aa1830822f9043ce9320859d48d59
Egor Nazdryuhin <egornnn@gmail.com>
2023-05-16 02:14:49 +0300

    Day 8: The Ceasar Cipher

--- Begginer/The Ceasar Cipher/main.py
+++ Begginer/The Ceasar Cipher/main.py
@@ -1,20 +1,11 @@
-def encode(message, shift):
+def ceasar(mode, message, shift):
     alphabet=list('abcdefghigklmnopqrstuvwxyz')
+    if mode=='decode': shift*=-1
     message_as_list=list(message.lower())
     for i in range(len (message_as_list)):
         if message_as_list[i] in alphabet:
             index = alphabet.index(message_as_list[i])
             index =(index+shift) % 26
-            message_as_list[i]=alphabet[index]
-    return "".join(message_as_list)
-
-def decode(message, shift):
-    alphabet=list('abcdefghigklmnopqrstuvwxyz')
-    message_as_list=list(message.lower())
-    for i in range(len (message_as_list)):
-        if message_as_list[i] in alphabet:
-            index = alphabet.index(message_as_list[i])
-            index =(index-shift) % 26
             message_as_list[i]=alphabet[index]
     return "".join(message_as_list)
 
@@ -28,10 +19,7 @@ def main_loop():
             if mode_program=='encode' or mode_program == 'decode' : is_correct=True
         message = input("Please, input the message to work:\n")
         shift = int(input('Please, provide the key (shift of the cipher):\n'))
-        if mode_program=='encode':
-            print(f'Your encoded message is {encode(message, shift)}')
-        elif mode_program=='decode':
-            print(f'Your decoded message is {decode(message, shift)}')
+        print(f'Your {mode_program}d message is {ceasar(mode_program, message, shift)}')
         message = input ("Do you wish to continue ('Yes' or 'No')?\n")
         if message.lower()=='no': is_continue=False
 
