7c759f13eccc5da2d9cee9d7fa85ed5a913081e3
Egor Nazdryuhin <egornnn@gmail.com>
2023-05-23 03:14:23 +0300

    Day 15: Coffee Machine

--- Intermediate/Coffee Machine/main.py
+++ Intermediate/Coffee Machine/main.py
@@ -30,10 +30,82 @@ resources = {
     "coffee": 100,
 }
 
-# TODO: 1 Prompt user by asking
-# TODO: 2 Turn off the Coffee Machine by entering “off” to the prompt
-# TODO: 3. Print report.
-# TODO: 4. Check resources sufficient?
-# TODO: 5. Process coins
-# TODO: 6. Check transaction successful?
-# TODO: 7. Make Coffee.
+VALID_OPTIONS = ["espresso", "latte", "cappuccino", "off", "report"]
+
+COINS = {
+    "quarter": 0.25,
+    "dime": 0.1,
+    "nickel": 0.05,
+    "pennies": 0.01,
+}
+
+
+def coffe_machine(stock: dict) -> None:
+
+    def print_resources(ingredients: dict, money: float) -> None:
+        for key in list(ingredients):
+            if key == "water" or key == "milk":
+                print(f"{key.capitalize()}: {ingredients[key]}ml" )
+            elif key == "coffe":
+                print(f"{key.capitalize()}: {ingredients[key]}g")
+            else:
+                print(f"{key.capitalize()}: {ingredients[key]}")
+        print(f"Money: ${'%.2f' % money}")
+
+    def check_enough(current_amount: dict, option: str) -> str:
+        requirements = MENU[option]["ingredients"]
+        for ingredient in list(requirements):
+            if requirements[ingredient] > current_amount[ingredient]:
+                return ingredient
+        return ""
+
+    def process_coins(cost: float):
+        inserted_coins = 0
+        print("Please insert coins")
+        for key in list(COINS):
+            number_of_coins = ""
+            while not number_of_coins.isdecimal():
+                number_of_coins = input(f"How many {key}: ")
+            inserted_coins += int(number_of_coins) * COINS[key]
+        if cost > inserted_coins:
+            return -1
+        else:
+            return inserted_coins
+
+    def update_resources(current_amounts: dict, option: str):
+        for key in MENU[option]['ingredients']:
+            current_amounts[key] -= MENU[option]['ingredients'][key]
+        return current_amounts
+
+    money = 0
+    current_resources = stock
+    while True:
+        prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        match prompt:
+            case "off":
+                return
+            case "report":
+                print_resources(current_resources, money)
+            case "espresso" | "latte" | "cappuccino":
+                enough_resource = check_enough(current_resources, prompt)
+                if enough_resource == "":
+                    payment = process_coins(MENU[prompt]["cost"])
+                    if payment == -1:
+                        print("Sorry that's not enough money. Money refunded.")
+                        continue
+                    else:
+                        change = payment - MENU[prompt]["cost"]
+                        print(f"Here is ${'%.2f' % change} in change.")
+                        money += MENU[prompt]["cost"]
+                        current_resources = update_resources(current_resources, prompt)
+                        print(f"Here is your {prompt} ☕️\nEnjoy!")
+                else:
+                    print(f'Sorry there is not enough {enough_resource}.')
+                    continue
+
+            case _:
+                print("incorrect input")
+
+
+coffe_machine(resources)
+
